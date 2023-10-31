d6cc9ecf37bbe3515e6ef77bded64b35a700ad9b
Mateus Schiavi <mtsschiavi@gmail.com>
2023-10-31 19:03:26 -0300

    added operators exercise

---
+++ operadores.php
@@ -0,0 +1,18 @@
+<?php
+
+$numero1 = 16;
+$numero2 = 8;
+
+$suma = $numero1 + $numero2;
+$resta = $numero1 - $numero2;
+$multiplicacion = $numero1 * $numero2;
+$division = $numero1 / $numero2;
+$modulo = $numero1 % $numero2;
+
+echo "La suma de $numero1 y $numero2 es: $suma<br>";
+echo "La resta de $numero1 y $numero2 es: $resta<br>";
+echo "La multiplicacion de $numero1 y $numero2 es: $multiplicacion<br>";
+echo "La división de $numero1 entre $numero2 es: $division<br>";
+echo "El módulo de la divisíon de $numero1 entre $numero2 es: $modulo<br>";
+
+?>
